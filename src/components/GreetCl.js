801b4f9aec0061a9dd24aa97af6ee6adae00955c
ppmaung <ppmaungdev@gmail.com>
2022-05-15 17:46:58 +0630

    react start

---
+++ src/components/GreetCl.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class GreetCl extends Component {
+    render() {
+        return <h1>Hello Class!</h1>
+    }
+}
+
+export default GreetCl
---
+++ src/components/GreetFunc.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// function Greet() {
+//     return <h1>Hello PPM</h1>
+// }
+
+const GreetFunc = () => <h1>Hello PPM!</h1>
+
+export default GreetFunc
