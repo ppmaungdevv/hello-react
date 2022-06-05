ac12416e3ec339cfe42056e1801e8b3e4b04de87
ppmaung <ppmaungdev@gmail.com>
2022-06-05 17:16:58 +0630

    context part 2 default value and consume with context type

--- src/components/ContextComponents/ComponentE.js
+++ src/components/ContextComponents/ComponentE.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
 import ComponentF from './ComponentF'
+import UserContext from './userContext'
 
 class ComponentE extends Component {
   render() {
     return (
       <div>
+          <div>
+            Comp E Context {this.context}
+          </div>
           <ComponentF />
       </div>
     )
   }
 }
+
+ComponentE.contextType = UserContext // context type usage
 
 export default ComponentE
--- src/components/ContextComponents/userContext.js
+++ src/components/ContextComponents/userContext.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-const UserContext = React.createContext()
+// const UserContext = React.createContext()
+const UserContext = React.createContext('Default Context')
 
 const UserProvider = UserContext.Provider
 const UserConsumer = UserContext.Consumer
@@ -10,6 +11,8 @@ export {
     UserConsumer
 }
 
+export default UserContext // for context type property
+
 // context is useful for deeply nested components
 // but it's not like VueX
 // every context come with Consumer and Provider React Components
@@ -17,3 +20,11 @@ export {
 // e.g 
 // if you use Provider in App, all components can consume the context 
 // if you use Provider in ComponentC, only E ann F can consume the context 
+
+// default value can be set in context, set while creating the context React.createContext('default')
+// default value appear when there is no matching provider (<UserProvider value='Pyae' >) in component tree
+
+// context can also be consume by context type property
+// draw backs of context type usage are
+// - It's only works with classes
+// - can only use with single context 
